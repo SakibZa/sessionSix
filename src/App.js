0078bf95ebf2251263322199a0655719337fbd43
sakibhusainzaidi2000@gmail.com <sakibhusainzaidi2000@gmail.com>
2024-03-13 13:25:45 +0530

    Completd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState, useEffect, useCallback } from "react";
 
-function App() {
+export default function App() {
+  const [url, setUrl] = useState("http://localhost:8000/products");
+  const [products, setProducts] = useState([]);
+
+  const fetchData = useCallback(async () => {
+    const response = await fetch(url);
+    const result = await response.json();
+
+    setProducts(result);
+  }, [url]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>All Products</h1>
+      <button
+        className="btn"
+        onClick={() => setUrl("http://localhost:8000/products?in_stock=1")}
+      >
+        Available
+      </button>
+      <button
+        className="btn"
+        onClick={() => setUrl("http://localhost:8000/products")}
+      >
+        AllProducts
+      </button>
+
+      <div className="productContainer">
+        {products &&
+          products.map((item) => (
+            <div className="product">
+              <h4>{item.name}</h4>
+              <div className="spanContainer">
+                <span> {item.in_stock ? "Available" : "Unavailable"}</span>
+
+                <span>Price : ${item.price}</span>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
-
-export default App;
